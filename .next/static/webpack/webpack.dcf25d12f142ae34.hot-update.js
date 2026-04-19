65344046330bab7c42a1d79d55178963d1b015bc
jz-clln <jabezcollano@gmail.com>
2026-04-19 18:39:36 +0800

    Fix PostCss

--- .next/static/webpack/webpack.dcf25d12f142ae34.hot-update.js
+++ .next/static/webpack/webpack.dcf25d12f142ae34.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a88baa25d35623b3"; }
+/******/ 	__webpack_require__.h = function() { return "85486baa00950b65"; }
 /******/ }();
 /******/ 
 /******/ }
